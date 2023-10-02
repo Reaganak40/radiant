010e7cb50f76280753a8830f12d407aff10929d1
Reagan <reagan.kelley@wsu.edu>
2023-10-02 16:32:56 -0400

    added form UI to launcher.

--- Launcher/Form1.Designer.cs
+++ Launcher/Form1.Designer.cs
@@ -28,18 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DirectoryLabel = new Label();
+            ProjectLabel = new Label();
+            ProjectPanel = new Panel();
+            NoLoadedProjectsLabel = new Label();
+            menuStrip1 = new MenuStrip();
+            NewProjectMenuButton = new ToolStripMenuItem();
+            LoadProjectMenuButton = new ToolStripMenuItem();
+            ProjectPanel.SuspendLayout();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // DirectoryLabel
+            // 
+            DirectoryLabel.AutoSize = true;
+            DirectoryLabel.ForeColor = SystemColors.ButtonFace;
+            DirectoryLabel.Location = new Point(12, 9);
+            DirectoryLabel.Name = "DirectoryLabel";
+            DirectoryLabel.Size = new Size(140, 20);
+            DirectoryLabel.TabIndex = 0;
+            DirectoryLabel.Text = "Radiant > Welcome";
+            // 
+            // ProjectLabel
+            // 
+            ProjectLabel.AutoSize = true;
+            ProjectLabel.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            ProjectLabel.ForeColor = SystemColors.ButtonFace;
+            ProjectLabel.Location = new Point(12, 44);
+            ProjectLabel.Name = "ProjectLabel";
+            ProjectLabel.Size = new Size(244, 81);
+            ProjectLabel.TabIndex = 1;
+            ProjectLabel.Text = "Projects";
+            // 
+            // ProjectPanel
+            // 
+            ProjectPanel.BackColor = Color.FromArgb(25, 25, 25);
+            ProjectPanel.BorderStyle = BorderStyle.FixedSingle;
+            ProjectPanel.Controls.Add(NoLoadedProjectsLabel);
+            ProjectPanel.Controls.Add(menuStrip1);
+            ProjectPanel.Location = new Point(22, 146);
+            ProjectPanel.Name = "ProjectPanel";
+            ProjectPanel.Size = new Size(706, 503);
+            ProjectPanel.TabIndex = 2;
+            // 
+            // NoLoadedProjectsLabel
+            // 
+            NoLoadedProjectsLabel.AutoSize = true;
+            NoLoadedProjectsLabel.ForeColor = SystemColors.ButtonFace;
+            NoLoadedProjectsLabel.Location = new Point(203, 253);
+            NoLoadedProjectsLabel.Name = "NoLoadedProjectsLabel";
+            NoLoadedProjectsLabel.Size = new Size(261, 20);
+            NoLoadedProjectsLabel.TabIndex = 1;
+            NoLoadedProjectsLabel.Text = "There are no currently loaded projects";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.Black;
+            menuStrip1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { NewProjectMenuButton, LoadProjectMenuButton });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(704, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "ProjectPanelMenuStrip";
+            // 
+            // NewProjectMenuButton
+            // 
+            NewProjectMenuButton.ForeColor = SystemColors.ButtonFace;
+            NewProjectMenuButton.Name = "NewProjectMenuButton";
+            NewProjectMenuButton.RightToLeft = RightToLeft.Yes;
+            NewProjectMenuButton.Size = new Size(103, 24);
+            NewProjectMenuButton.Text = "New Project";
+            NewProjectMenuButton.Click += NewProjectMenuButton_Click;
+            // 
+            // LoadProjectMenuButton
+            // 
+            LoadProjectMenuButton.ForeColor = SystemColors.ButtonFace;
+            LoadProjectMenuButton.Name = "LoadProjectMenuButton";
+            LoadProjectMenuButton.Size = new Size(106, 24);
+            LoadProjectMenuButton.Text = "Load Project";
             // 
             // Launcher
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(37, 37, 38);
+            ClientSize = new Size(1262, 673);
+            Controls.Add(ProjectPanel);
+            Controls.Add(ProjectLabel);
+            Controls.Add(DirectoryLabel);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = menuStrip1;
+            MaximizeBox = false;
             Name = "Launcher";
-            Text = "Radiant - New Project";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            ProjectPanel.ResumeLayout(false);
+            ProjectPanel.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label DirectoryLabel;
+        private Label ProjectLabel;
+        private Panel ProjectPanel;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem NewProjectMenuButton;
+        private ToolStripMenuItem LoadProjectMenuButton;
+        private Label NoLoadedProjectsLabel;
     }
 }
--- Launcher/Form1.cs
+++ Launcher/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Launcher
 {
     public partial class Launcher : Form
@@ -5,6 +7,20 @@ namespace Launcher
         public Launcher()
         {
             InitializeComponent();
+        }
+
+        [DllImport("DwmApi")] //System.Runtime.InteropServices
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            if (DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4) != 0)
+                DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+        }
+
+        private void NewProjectMenuButton_Click(object sender, EventArgs e)
+        {
+            this.Text = "Click worked!";
         }
     }
 }
